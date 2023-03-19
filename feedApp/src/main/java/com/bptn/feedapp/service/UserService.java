3610190d8b3539be8236b5f8c36d97a3f54065a9
digitalmmm <aelmi416@gmail.com>
2023-03-19 14:25:05 -0400

    FeedApp

---
+++ feedApp/src/main/java/com/bptn/feedapp/service/UserService.java
@@ -0,0 +1,54 @@
+package com.bptn.feedapp.service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedapp.jpa.User;
+import com.bptn.feedapp.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	EmailService emailService;
+	
+	
+	public List<User> listUsers() {
+		
+		return this.userRepository.findAll();
+		
+		
+	}
+	
+	public Optional<User> findByUsername(String username) {
+		
+		return this.userRepository.findByUsername(username);
+	}
+	
+	public void createUser(User user) {
+		
+		this.userRepository.save(user);
+	}
+	
+	public User signup(User user){
+		
+		user.setUsername(user.getUsername().toLowerCase());
+		user.setEmailId(user.getEmailId().toLowerCase());
+		user.setEmailVerified(false);
+		user.setCreatedOn(Timestamp.from(Instant.now()));
+		
+		this.emailService.sendVerificationEmail(user);
+		
+		this.userRepository.save(user);
+		return user;
+		
+	}
+}
